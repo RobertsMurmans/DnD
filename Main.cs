d501cd34c5cef3f0bc76c8dcf2417229adf53d44
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2023-09-11 14:20:46 +0300

    Setting up main file

---
+++ Main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Text;
+
+
+
+function startup(){
+
+    string path = "C:\Users\Roberts\AppData\Roaming\DnD\Variables.dat"
+
+    if(!File.Exists(path)){
+        
+        var seed = 0;
+        return true;
+
+    }
+    
+    public static System.IO.FileStream Create (string path);
+    return true
+
+}
+
+void Main(){
+    startup();
+
+}
--- Startup.cs
+++
@@ -1,1 +0,0 @@
-var setup = true;
